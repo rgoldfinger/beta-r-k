15afe22d3cb1619b9100b981e684040e21c0610b
hungrycat <rgoldfinger@gmail.com>
2014-07-11 11:15:51 -0700

    Initial Commit

---
+++ should I run? draftTests/should_I_run__draftTests.swift
@@ -0,0 +1,35 @@
+//
+//  should_I_run__draftTests.swift
+//  should I run? draftTests
+//
+//  Created by Roger Goldfinger on 7/11/14.
+//  Copyright (c) 2014 Roger Goldfinger. All rights reserved.
+//
+
+import XCTest
+
+class should_I_run__draftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
